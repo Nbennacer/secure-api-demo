b5aa9ec9f36ca60429be7ef897cde55c2bf5cb16
Nour bennacer <ben93nour@gmail.com>
2025-12-25 19:16:26 -0500

    Initial Spring Boot project setup

---
+++ src/test/java/com/demo/secureapi/SecureApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.secureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
